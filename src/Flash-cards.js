1fe4b198d46f453c63c2da06eb0c21afadeab60d
breno-aredes <breno_mg10@hotmail.com>
2022-11-29 15:06:06 -0300

    v 0.1.1 componentização JSX

---
+++ src/Flash-cards.js
@@ -0,0 +1,18 @@
+export default function FlashCards() {
+    return (
+        <>
+            <div class="card">
+                <h2>Perguntar 1</h2>
+            </div>
+            <div class="card">
+                <h2>Perguntar 2</h2>
+            </div>
+            <div class="card">
+                <h2>Perguntar 3</h2>
+            </div>
+            <div class="card">
+                <h2>Perguntar 4</h2>
+            </div>
+        </>
+    )
+}
---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            0/4 CONCLUÍDOS
+        </footer>
+    )
+}
