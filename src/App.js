1a28c4a5fa3474698f0cb76cbb880fb6bff0a7e3
breno-aredes <breno_mg10@hotmail.com>
2022-11-29 14:34:23 -0300

    v 0.1 iniciando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 function App() {
   return (
     <>
+      <h1>zaprecall</h1>
+      <div class="card">
+        <h2>Perguntar 1</h2>
+      </div>
+      <div class="card">
+        <h2>Perguntar 2</h2>
+      </div>
+      <div class="card">
+        <h2>Perguntar 3</h2>
+      </div>
+      <div class="card">
+        <h2>Perguntar 4</h2>
+      </div>
 
+      <footer>
+        0/4 CONCLUÍDOS
+      </footer>
     </>
   );
 }
